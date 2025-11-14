c7b38ae58c3b82e948ce7c18812463ad88a060da
Daniel <daniel@MacBook-Air-Daniel.local>
2025-11-15 02:13:37 +0700

    model

---
+++ app/Models/produk.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class produk extends Model
+{
+    //inisialisasi tabel
+    protected $table="tb_produk";
+    //inisialisasi primary key
+    protected $primaryKey = 'id_produk';
+    //inisialisasi data yang boleh diisi
+    protected $fillable = ['nama_produk', 'harga_produk', 'stok_produk'];
+//inisialisasi data yang tidak boleh diisi
+    protected $guarded =['id_produk'];
+}
